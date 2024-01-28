bf22563fa934111919a597f19b514965ba58038b
Lucas Tarantino <tarantinolucas@hotmail.com>
2024-01-28 18:47:37 -0300

    Basic Javascript - Role Play (Ejercicios del 35 al 50).

--- javascriptBasico/juegoDeRol/script.js
+++ javascriptBasico/juegoDeRol/script.js
@@ -28,3 +28,44 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+
+// declaracion de funciones
+function goTown() {
+  button1.innerText = "Go to store";
+  button2.innerText = "Go to cave";
+  button3.innerText = "Fight dragon";
+  button1.onclick = goStore;
+  button2.onclick = goCave;
+  button3.onclick = fightDragon;
+  text.innerText =
+    'You are in the town square. You see a sign that says "Store".';
+}
+
+function goStore() {
+  button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
+  text.innerText = "You enter the store.";
+}
+
+function goCave() {
+  console.log("Going to cave.");
+}
+
+function fightDragon() {
+  console.log("Fighting dragon.");
+}
+
+function buyHealth() {}
+
+function buyWeapon() {}
+
+// inicializando botones
+
+// accedemos al button1 por notacion de puntos, y tomamos el evento "onclick" para ejecutar una funcion determinada
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
