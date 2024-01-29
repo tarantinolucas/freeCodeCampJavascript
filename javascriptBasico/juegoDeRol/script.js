4f8c92b9fdb2f3ff5ff0247eefbaa06375665549
Lucas Tarantino <tarantinolucas@hotmail.com>
2024-01-29 07:10:57 -0300

    Basic Javascript - Role Play (Ejercicios del 51 al 75).

--- javascriptBasico/juegoDeRol/script.js
+++ javascriptBasico/juegoDeRol/script.js
@@ -28,41 +28,81 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+// los arrays pueden recibir muchos datos, incluyendo objetos. Los objetos son elementos que cuenta con una "clave: valor". La clave tambien conocida como propiedad, cuando la propiedad tiene mas de una palabra se escribe entre "comillas".
+const locations = [
+  {
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon],
+    text: 'You are in the town square. You see a sign that says "Store".',
+  },
+  {
+    name: "store",
+    "button text": [
+      "Buy 10 health (10 gold)",
+      "Buy weapon (30 gold)",
+      "Go to town square",
+    ],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store.",
+  },
+  {
+    name: "cave",
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    "button functions": [fightSlime, fightBeast, goTown],
+    text: "You enter the cave. You see some monsters.",
+  },
+];
 
 // declaracion de funciones
+
+// creamos una funcion que reciba un parametro para que sea reutilizable
+
+function update(location) {
+  // utilizamos notacion de corchetes para acceder a la posicion de los array
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
+  // utilizamos notacion de puntos para acceder a la propiedad del objeto
+  text.innerText = location.text;
+}
+
 function goTown() {
-  button1.innerText = "Go to store";
-  button2.innerText = "Go to cave";
-  button3.innerText = "Fight dragon";
-  button1.onclick = goStore;
-  button2.onclick = goCave;
-  button3.onclick = fightDragon;
-  text.innerText =
-    'You are in the town square. You see a sign that says "Store".';
+  update(locations[0]);
 }
 
 function goStore() {
-  button1.innerText = "Buy 10 health (10 gold)";
-  button2.innerText = "Buy weapon (30 gold)";
-  button3.innerText = "Go to town square";
-  button1.onclick = buyHealth;
-  button2.onclick = buyWeapon;
-  button3.onclick = goTown;
-  text.innerText = "You enter the store.";
+  update(locations[1]);
 }
 
 function goCave() {
-  console.log("Going to cave.");
+  update(locations[2]);
 }
 
 function fightDragon() {
   console.log("Fighting dragon.");
 }
 
-function buyHealth() {}
+function buyHealth() {
+  gold -= 10;
+  health += 10;
+  goldText.innerText = gold;
+  healthText.innerText = health;
+}
 
-function buyWeapon() {}
+function buyWeapon() {
+  console.log("Buying weapon.");
+}
 
+function fightSlime() {
+  console.log("Fighting slime.");
+}
+function fightBeast() {
+  console.log("Fighting beast.");
+}
 // inicializando botones
 
 // accedemos al button1 por notacion de puntos, y tomamos el evento "onclick" para ejecutar una funcion determinada
