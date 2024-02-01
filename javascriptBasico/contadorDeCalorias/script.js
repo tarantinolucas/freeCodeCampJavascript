079492b2f09b725ebf83d0cff8334986aee40378
Lucas Tarantino <tarantinolucas@hotmail.com>
2024-02-01 18:57:44 -0300

    Validacion de formularios - Contador de calorias (Ej. del 15 al 25).

--- javascriptBasico/contadorDeCalorias/script.js
+++ javascriptBasico/contadorDeCalorias/script.js
@@ -11,12 +11,13 @@ let isError = false;
 // creamos una funcion para captrar un string y convertirlo en un numero
 function cleanInputString(str) {
   // el metodo split separa en partes un string en un nuevo array de substrings segun el argumento de separacion que le coloquemos
-  //   const strArray = str.split("");
-  //   const cleanStrArray = [];
-  //   for (let i = 0; i < strArray.length; i++) {
-  //     if (!["+", "-", " "].includes(strArray[i])) {
-  //       cleanStrArray.push(strArray[i]);
-  //     }
-  //   }
-  const regex = /\+-\s/;
+  // utilizamos expresiones regulares para filtrar strings y que no tengan simbolos +, - o espacios vacios.
+  const regex = /[+-\s]/g;
+  // utilizamos el metodo replace() para reemplazar patrones de la expresion regular definida por caracteres vacios
+  return str.replace(regex, "");
+}
+
+// creamos una funcion para evitar las notaciones exponenciales
+function isInvalidInput(str) {
+  const regex = /e/;
 }
