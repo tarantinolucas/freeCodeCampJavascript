dd62b2b98bcd899fcc8534099d41fa7fabb70e2a
Lucas Tarantino <tarantinolucas@hotmail.com>
2024-02-04 22:49:13 -0300

    Validacion de formularios - Contador de calorias (Ej. del 46 al 50).

--- javascriptBasico/contadorDeCalorias/script.js
+++ javascriptBasico/contadorDeCalorias/script.js
@@ -23,7 +23,6 @@ function isInvalidInput(str) {
   return str.match(regex);
 }
 
-
 // funcion para que el usuario ingrese datos
 function addEntry() {
   const targetInputContainer = document.querySelector(
@@ -31,5 +30,12 @@ function addEntry() {
   );
   const entryNumber =
     targetInputContainer.querySelectorAll('input[type="text"]').length;
-  const HTMLString = `<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>`;
+  const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"></input>
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input type="number" min="0" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories" />`;
+  targetInputContainer.innerHTML += HTMLString;
 }
+
+addEntryButton.addEventListener("click", addEntry);
