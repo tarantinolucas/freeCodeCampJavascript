b86679b454e56228e12f9d2d2169efe5deaaf2f4
Lucas Tarantino <tarantinolucas@hotmail.com>
2024-02-22 06:55:51 -0300

    Comenzamos con proyecto: Reproductor de musica (metodos basicos de array y strings. (Ej. del 1 al 5).

---
+++ javascriptBasico/reproductorDeMusica/script.js
@@ -0,0 +1,33 @@
+// In this project you will learn basic string and array methods by building a music player app. You will be able to play, pause, skip, and shuffle songs.
+
+// Let's start
+
+// En este proyecto vamos a aprender metodos basicos de arrays y strings creando un reproductor de musica. Seremos capaz de reproducir, pausar, saltar y  escuchar aleatoriamente canciones.
+
+// Comenzamos!
+
+// Capturando los botones de la pagina
+const playlistSongs = document.getElementById("playlist-songs");
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause");
+const nextButton = document.getElementById("next");
+const previousButton = document.getElementById("previous");
+const shuffleButton = document.getElementById("shuffle");
+
+// Array para guardar canciones
+const allSongs = [
+  {
+    id: 0,
+    title: "Scratching The Surface",
+    artist: "Quincy Larson",
+    duration: "4:25",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/scratching-the-surface.mp3",
+  },
+  {
+    id: 1,
+    title: "Can't Stay Down",
+    artist: "Quincy Larson",
+    duration: "4:15",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stay-down.mp3",
+  },
+];
