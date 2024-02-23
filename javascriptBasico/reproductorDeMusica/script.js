41a53b28c1c12b68bbf1367574660bc1da1c6f10
Lucas Tarantino <tarantinolucas@hotmail.com>
2024-02-23 19:01:21 -0300

    Reproductor de musica - Ej. del 6 al 10.

--- javascriptBasico/reproductorDeMusica/script.js
+++ javascriptBasico/reproductorDeMusica/script.js
@@ -1,10 +1,10 @@
 // In this project you will learn basic string and array methods by building a music player app. You will be able to play, pause, skip, and shuffle songs.
 
-// Let's start
+// Let's start!
 
 // En este proyecto vamos a aprender metodos basicos de arrays y strings creando un reproductor de musica. Seremos capaz de reproducir, pausar, saltar y  escuchar aleatoriamente canciones.
 
-// Comenzamos!
+// Empecemos!
 
 // Capturando los botones de la pagina
 const playlistSongs = document.getElementById("playlist-songs");
@@ -30,4 +30,22 @@ const allSongs = [
     duration: "4:15",
     src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stay-down.mp3",
   },
+  {
+    id: 2,
+    title: "Still Learning",
+    artist: "Quincy Larson",
+    duration: "3:51",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/still-learning.mp3",
+  },
 ];
+
+// creamos un elemento de audio HTML5
+
+const audio = new Audio();
+
+// informacion del usuario
+let userData = {
+  songs: [...allSongs],
+  currentSong: null,
+  songCurrentTime: 0,
+};
