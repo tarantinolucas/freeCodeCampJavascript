10f0b0c93c337f80d5390b453bcafc08d678c2bd
Lucas Tarantino <tarantinolucas@hotmail.com>
2024-03-25 07:30:43 -0300

    Proyecto: Convertidor decimal a binario (Ej. 71 al 85).

--- javascriptModerno/convertidorDecimalABinario/script.js
+++ javascriptModerno/convertidorDecimalABinario/script.js
@@ -3,6 +3,18 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+const animationData = [
+  {
+    inputVal: 5,
+    marginTop: 300,
+    addElDelay: 1000,
+  },
+  {
+    inputVal: 2,
+    marginTop: -200,
+    addElDelay: 1500,
+  },
+];
 
 /* 
 
@@ -27,8 +39,10 @@ countDownAndUp(3);
 
 // funcion para convertir el dato ingresado a binario
 const decimalToBinary = (input) => {
-  if (input === 0) {
-    return "";
+  // aplicamos recursividad sobre el dato ingresado
+  if (input === 0 || input === 1) {
+    // agregamos dos casos bases, si es 0 o 1 convertir el dato en string y devolverlo
+    return String(input);
   } else {
     return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
@@ -48,17 +62,42 @@ const decimalToBinary = (input) => {
   */
 };
 
+// funcion para mostrar paso a paso del programa
+const showAnimation = () => {
+  // agregamos 500ms a que muestre free
+  setTimeout(() => {
+    console.log("free");
+  }, 500);
+  // agregamos tiempo con setTimeout para que demore 1 segundo en mostrar el mensaje de "Code"
+  setTimeout(() => {
+    console.log("Code");
+  }, 1000);
+  // agregamos 1.5s para que muestre "Camp"
+  setTimeout(() => {
+    console.log("Camp");
+  }, 1500);
+};
+
 // funcion para ver el dato ingresado por el usuario
 const checkUserInput = () => {
   // en JS hay algunos valores que son comunmente falsos (los datos nulos, indefinidos, el numero 0 y strings vacias). Aca utilizamos el operador logico NOT para comprobar si el valor es falso
 
+  // convertimos la entrada en un entero para usarlo durante el algoritmo
+  const inputInt = parseInt(numberInput.value);
+
   // alertamos al usuario cuando lo que introduce no es un numero decimal
-  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+  if (!numberInput.value || isNaN(inputInt)) {
     alert("Please provide a decimal number");
     return;
   }
-  decimalToBinary(parseInt(numberInput.value));
-  result.textContent = decimalToBinary();
+
+  // vamos a analizar el caso cuando el usuario ingrese el numero 5
+  if (inputInt === 5) {
+    showAnimation();
+    return;
+  }
+
+  result.textContent = decimalToBinary(inputInt);
   // limpiamos el input de entrada
   numberInput.value = "";
 };
