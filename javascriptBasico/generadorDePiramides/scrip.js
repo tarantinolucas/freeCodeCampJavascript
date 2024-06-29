2bf33102bdc2935b78f7f731f1910cd076a90a50
Lucas Tarantino <tarantinolucas@hotmail.com>
2024-06-29 12:05:44 -0300

    Javascript introductorio - Generador de piramides (ej. del 1 al 23).

---
+++ javascriptBasico/generadorDePiramides/scrip.js
@@ -0,0 +1,40 @@
+/* declaracion, asignacion de strings en variables y muestra de variables por consola (hasta ej. 12)
+
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+secondCharacter = character;
+console.log(secondCharacter);
+
+*/
+
+/* declaracion y muestra por consola de variables (hasta ej. 15)
+
+let profession = "teacher";
+let age;
+console.log(age);
+console.log(profession);
+
+*/
+
+/* asignacion de numeros a variables declaradas y operaciones (hasta ej. 17)
+
+let count = 8;
+console.log(count + 1);
+console.log(count - 1);
+console.log(count / 2);
+console.log(count * 2);
+
+*/
+
+
+/* asignacion de arrays a una variable (hasta ej. 23)
+
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
+rows[2] = 10;
+console.log(rows)
+rows[rows.length - 1] = 10;
+
+*/
