dbe53e1718b7e101620797d7675f5aec3b1dbca0
Lucas Tarantino <tarantinolucas@hotmail.com>
2024-07-15 19:19:09 -0300

    Fundamentos de Javascript - Cambiador de color de fondo random (Ej. 1 y 2).

---
+++ javascriptBasico/cambiadorDeColorDeFondoRandom/script.js
@@ -0,0 +1,16 @@
+const darkColorsArr = [
+  "#2C3E50",
+  "#34495E",
+  "#2C2C2C",
+  "#616A6B",
+  "#4A235A",
+  "#2F4F4F",
+  "#0E4B5A",
+  "#36454F",
+  "#2C3E50",
+  "#800020",
+];
+function getRandomIndex() {
+  console.log(darkColorsArr.length * Math.random())
+}
+getRandomIndex();
