c2d99581591aa9bde4cf983683beb9ef23767ca2
Lucas Tarantino <tarantinolucas@hotmail.com>
2024-11-29 18:51:52 -0300

    Finalización de proyecto: Convertidor de números romanos.

--- javascriptModerno/convertidorNumerosRomanos/script.js
+++ javascriptModerno/convertidorNumerosRomanos/script.js
@@ -3,30 +3,74 @@
 const number = document.getElementById("number");
 const convertBtn = document.getElementById("convert-btn");
 const output = document.getElementById("output");
+const arabigos = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+const romanos = [
+  "M",
+  "CM",
+  "D",
+  "CD",
+  "C",
+  "XC",
+  "L",
+  "XL",
+  "X",
+  "IX",
+  "V",
+  "IV",
+  "I",
+];
 
 // funcion para ver el dato ingresado por el usuario
-const checkUserInput = () => {
+const checkUserInput = (number) => {
   isEmpty(number);
-  output.classList.toggle("hidden");
-  // limpiamos el input de entrada
-  number.value = "";
+  number = "";
 };
 
 // funcion que verifica si la entrada esta vacia
 const isEmpty = () => {
-  number.value == ""
-    ? (output.textContent = `Please enter a valid number`)
-    : (output.textContent = `El boton convert o la tecla enter fue pulsada y se estan procesando datos...`);
+  if (number.value == "") {
+    output.textContent = `Please enter a valid number`;
+    return;
+  } else {
+    isZeroOrLess(number);
+    return;
+  }
 };
 
-// capturamos cuando se hace click en convert-btn o enter en la pestaña
-
-// evento click sobre convert-btn
-convertBtn.addEventListener("click", checkUserInput);
-
-// enter sobre la pestaña
-number.addEventListener("keydown", (e) => {
-  if (e.key == "Enter") {
-    checkUserInput();
+// funcion que verifica si la entrada es 0 o un numero menor
+const isZeroOrLess = () => {
+  if (number.value <= 0) {
+    output.textContent = `Please enter a number greater than or equal to 1`;
+    return;
+  } else {
+    isGreaterNumber(number);
+    return;
   }
-});
+};
+
+// funcion que verifica si la entrada es 3999 o un numero mayor
+const isGreaterNumber = () => {
+  if (number.value >= 4000) {
+    output.textContent = `Please enter a number less than or equal to 3999`;
+    return;
+  } else {
+    convertToRoman(number);
+    return;
+  }
+};
+
+// funcion que verifica el numero y lo convierte
+const convertToRoman = (number) => {
+  let result = "";
+  let num = number.value
+  for (let i = 0; i < arabigos.length; i++) {
+    while (num >= arabigos[i]) {
+      num -= arabigos[i];
+      result += romanos[i];
+    }
+  }
+  output.textContent = result;
+};
+
+// capturamos el evento click sobre convert-btn
+convertBtn.addEventListener("click", checkUserInput);
