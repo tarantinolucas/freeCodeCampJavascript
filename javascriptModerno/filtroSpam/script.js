07b4fd9a1b257e28c55c2ac6635e3c1b1befb45b
Lucas Tarantino <tarantinolucas@hotmail.com>
2024-12-01 20:14:00 -0300

    Se completaron los ejercicios del 9 al 21 incluidos.

--- javascriptModerno/filtroSpam/script.js
+++ javascriptModerno/filtroSpam/script.js
@@ -5,10 +5,21 @@ const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
 // Creamos variables con las expresiones regulares para filtrar
-const helpRegex = /please help/i;
+const helpRegex = /please help|assist me/i;
+// Capturamos frases que inicien con un número con 1 o mas digitos, que luego tengan cero o mas espacios, que incluyan o no algunas de las palabras incluidas entre parentesis, 
+const dollarRegex = /[0-9]+\s*(?:hundred|thousand|million|billion)?\s+dollars/i; 
 
-// Funcion para verificar si es spam
-const isSpam = (msg) => msg.match(helpRegex);
+// Creamos un array de expresiones regulares
+const denyList = [helpRegex, dollarRegex];
+
+/* Funcion para verificar si es spam
+const isSpam = (msg) => msg.match(helpRegex); */
+
+/* Lo verificamos pero con el metodo test().
+const isSpam = (msg) => helpRegex.test(msg); */
+
+// Usamos el método some() para verificar si alguno de los elementos del mensaje coincide con algunos de las expresiones regulares del array denyList.
+const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
 
 // Capturamos el evento click sobre el boton "Check Message"
 checkMessageButton.addEventListener("click", () => {
