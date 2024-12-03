d15893e8eea5f6bedca2284e47a9fdcb83242def
Lucas Tarantino <tarantinolucas@hotmail.com>
2024-12-02 22:21:06 -0300

    Se realizaron ejercicios desde el N° 17 al 32.

--- javascriptModerno/clasificadorDeNumeros/script.js
+++ javascriptModerno/clasificadorDeNumeros/script.js
@@ -8,8 +8,57 @@ const sortInputArray = (event) => {
   const inputValues = [
     ...document.getElementsByClassName("values-dropdown"),
   ].map((dropdown) => Number(dropdown.value)); // Usamos Number() para convertir los valores en numero
-  updateUI(inputValues);
+  const sortedValues = selectionSort(inputValues);
+  // Usamos el array ya ordenado para mostrarlo
+  updateUI(sortedValues);
 };
+
+/* ----------------------------------------------------- */
+/* Funcion para ordenar los datos por metodo de burbujeo */
+/* ----------------------------------------------------- */
+// Metodo inactivo, se esta usando el metodo de seleccion
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      // Este console.log permite ver como se van comparando y acomodando los numeros
+      console.log(array, array[j], array[j + 1]);
+      // Comparamos dos elementos, si a es mayor que b, entonces se cambian de posicion, se repite el proceso hasta que todos esten ordenados
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+};
+/* ----------------------------------------------------- */
+/* Fin de la funcion ----------------------------------- */
+/* ----------------------------------------------------- */
+
+
+
+/* ------------------------------------------------------ */
+/* Funcion para ordenar los datos por metodo de seleccion */
+/* ------------------------------------------------------ */
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < array.length; j++) {
+      console.log(array, array[j], array[minIndex]);
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+    const temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+  }
+  return array;
+};
+/* ----------------------------------------------------- */
+/* Fin de la funcion ----------------------------------- */
+/* ----------------------------------------------------- */
 
 // Capturamos el click sobre el boton Sort
 sortButton.addEventListener("click", sortInputArray);
