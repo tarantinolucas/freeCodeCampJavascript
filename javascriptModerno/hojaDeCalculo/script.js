b7e3ce99a4cc581b6e0f053aa434eafed4f1f4fe
Lucas Tarantino <tarantinolucas@hotmail.com>
2024-12-07 16:19:55 -0300

    Proyecto Hoja de cálculo: Ejercicios del 51 al 55  inclusive.

--- javascriptModerno/hojaDeCalculo/script.js
+++ javascriptModerno/hojaDeCalculo/script.js
@@ -46,7 +46,11 @@ const evalFormula = (x, cells) => {
   const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
   const elemValue = (num) => (character) => idToText(character + num);
   const addCharacters = (character1) => (character2) => (num) =>
-    charRange(character1, character2).map();
+    charRange(character1, character2).map(elemValue(num));
+  const rangeExpanded = x.replace(
+    rangeRegex,
+    (match, char1, num1, char2, num2) => {}
+  );
 };
 
 // Utilizamos el metodo onload de windows para ejecutar funciones cuando se carga la página
