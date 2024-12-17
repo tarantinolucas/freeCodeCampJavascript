1f1b45f324e96a098e8627595de21c4d26f29574
Lucas Tarantino <tarantinolucas@hotmail.com>
2024-12-17 20:23:49 -0300

    Proyecto Hoja de cálculo: Finalizado. Ejercicios del 97 al 105 inclusive.

--- javascriptModerno/hojaDeCalculo/script.js
+++ javascriptModerno/hojaDeCalculo/script.js
@@ -43,10 +43,20 @@ const median = (nums) => {
 /* ============================================ */
 
 const spreadsheetFunctions = {
+  "": (arg) => arg,
   sum,
   average,
   median,
   even: (nums) => nums.filter(isEven),
+  someeven: (nums) => nums.some(isEven),
+  everyeven: (nums) => nums.every(isEven),
+  firsttwo: (nums) => nums.slice(0, 2),
+  lasttwo: (nums) => nums.slice(-2),
+  has2: (nums) => nums.includes(2),
+  increment: (nums) => nums.map((num) => num + 1),
+  random: ([x, y]) => Math.floor(Math.random() * y + x),
+  range: (nums) => range(...nums),
+  nodupes: (nums) => [...new Set(nums).values()],
 };
 
 const applyFunction = (str) => {
